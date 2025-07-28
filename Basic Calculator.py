11d0abeeda76fbbb69c0e1198bb4d59352c9d77c
Shazib4nwer <ansarishazib876@gmail.com>
2025-07-28 15:02:25 +0530

    1st Project 🫡

---
+++ Basic Calculator.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your 1st number:"))
+b = int(input("Enter your 2nd number:"))
+c = (input("Enter your operand{+ - * /}:"))
+if c == "+":
+    print("The sum is:",a+b)
+if c == "-":
+    print("The difference is:",a-b)
+if c == "*":
+    print("The product is:",a*b)
+if c == "/":
+    print("The quotient is:",a/b)
